cd7ecc0ab4de203018a4ab6defa0c2405e21e1a1
Mozi <iilaydaokan@gmail.com>
2022-10-20 23:12:41 +0300

    change baseURl

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://34.125.176.136:3000/"
+  baseURL: "http://localhost:8080/"
 });
 
 instance.defaults.headers.authorization = `Bearer ${localStorage.getItem("token") || ""}`;
